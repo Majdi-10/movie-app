cff8788b1ba876e9701de70e551f759ed5965a68
Majdi-10 <majdi10_joiblia10@hotmail.com>
2021-07-16 11:57:43 +0100

    intialisation of the app with implmentig the front end sideand the back end side

---
+++ movies/server/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+
+
+app.use(express.json())
+app.use(express.static(__dirname + "/../client/build"));
+app.use(express.urlencoded({ extended: true }));
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
+
